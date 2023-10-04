56317821483a5b89c7f604cb556e1a0e1c25a3b1
Mahesh <90019194+MaheshKariappa@users.noreply.github.com>
2023-10-04 19:52:39 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,11 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Mahesh Technologies");
+		js.put("Calling Name", "Mahesh");
+		js.put("DOB", "12-07-1994");
+		js.put("Hobbies", "Kicking-Head off Cycling Drinking beer..");
+		js.put("Places he like", "Amsterdam-But didnt go there");
 
 		return js.toString();
 }
